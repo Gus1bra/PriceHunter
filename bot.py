ca1db440bac1d3395def38155583ec264d9f62ce
gus1bra <gus1bra@mail.ru>
2025-04-18 16:27:32 +0300

    ФИКСЫ

--- bot.py
+++ bot.py
@@ -1,29 +1,21 @@
 import asyncio
 import sys
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from config import BOT_TOKEN
+from handlers import commands, forward_handler
 
 # Установка правильного цикла событий для Windows
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties  # Импортируем DefaultBotProperties
-from aiogram.enums import ParseMode
-import asyncio
-from config import BOT_TOKEN
-from handlers import commands, forward_handler
-
-# Создаем объект DefaultBotProperties с нужным parse_mode
-default_properties = DefaultBotProperties(parse_mode=ParseMode.HTML)
-
-# Передаем default_properties в конструктор Bot
-bot = Bot(token=BOT_TOKEN, default=default_properties)
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 # Регистрация роутеров
 dp.include_router(commands.router)
 dp.include_router(forward_handler.router)
 
-# Точка входа
 async def main():
     await dp.start_polling(bot)
 
--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-# Загрузка переменных окружения
 load_dotenv()
 
-# Конфигурация
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DB_PATH = os.getenv("DB_PATH", "price_hunter.db")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
